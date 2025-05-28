6e90d82374cc95c41ddef681cf194d50808ca827
Divyanshu <divyanshuvishwakarma222@gmail.com>
2025-05-28 19:29:23 +0530

    remove code

--- src/components/global/navbar/navbar.tsx
+++ src/components/global/navbar/navbar.tsx
@@ -9,10 +9,10 @@ import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
+  // SheetDescription,
   SheetFooter,
   SheetHeader,
-  SheetTitle,
+  // SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button";
@@ -108,10 +108,7 @@ const Navbar = () => {
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you're done.
-          </SheetDescription>
+          
         </SheetHeader>
         <div className="grid gap-4 py-4">
           
