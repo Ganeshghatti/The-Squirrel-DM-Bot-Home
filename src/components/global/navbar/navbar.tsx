e9b290bdf46eb0189332ace1bdc5d5a4bea3179a
Divyanshu <divyanshuvishwakarma222@gmail.com>
2025-05-31 00:13:58 +0530

    remove imports

--- src/components/global/navbar/navbar.tsx
+++ src/components/global/navbar/navbar.tsx
@@ -19,17 +19,9 @@ import Image from "next/image";
 
 import {
   Menubar,
-  MenubarCheckboxItem,
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarRadioGroup,
-  MenubarRadioItem,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarSub,
-  MenubarSubContent,
-  MenubarSubTrigger,
   MenubarTrigger,
 } from "@/components/ui/menubar";
 
